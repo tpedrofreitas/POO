3edd9c45af242287139606dc8517384536b18d30
Thiago Freitas <thifreitas26@gmail.com>
2025-09-15 22:25:22 -0300

    Aluno

---
+++ 08_Aluno/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Aluno
+{
+    internal class Aluno
+    {
+        private string nome;
+        private int idade;
+        private double nota1;
+        private double nota2;
+
+        public Aluno(string nome, int idade,double nota1, double nota2)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.nota1 = nota1;
+            this.nota2 = nota2;
+        }
+        public 
+
+
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
