430a82398ffde989434ede670f817f0d8ed68517
Thiago Freitas <thifreitas26@gmail.com>
2025-09-26 19:45:08 -0300

    13_Coleção Stack

---
+++ 13_ColecaoStack/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_ColecaoStack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> pilha = new Stack<int>();
+
+            Console.WriteLine("Digite 5 números inteiros:");
+
+            for(int i = 1;  i <= 5; i++)
+            {
+                Console.Write($"Número {i}: ");
+                int numero = Convert.ToInt32(Console.ReadLine());
+                pilha.Push(numero);
+            }
+            Console.WriteLine($"O último número inserido foi:{pilha.Pop()}");
+        }
+    }
+}
