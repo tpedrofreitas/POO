a9f6307081c24ac48c3513354f4940f4c7d1140b
Thiago Freitas <thifreitas26@gmail.com>
2025-09-26 19:56:43 -0300

    14_Coleção Hash Set

---
+++ 14_ColecaoHashSet/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_ColecaoHashSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> numeros = new HashSet<int>();
+
+            numeros.Add(5);
+            numeros.Add(10);
+            numeros.Add(15);
+            numeros.Add(20);
+            numeros.Add(25);
+            numeros.Add(30);
+            numeros.Add(5);
+            numeros.Add(10);
+            numeros.Add(15);
+            numeros.Add(25);
+            numeros.Add(30);
+
+            Console.WriteLine("Lista final sem duplicatas:");
+
+            foreach (int n in numeros)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
