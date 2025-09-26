bb2cdef4eee42e52a1f25bd3422659ed5c53c685
Thiago Freitas <thifreitas26@gmail.com>
2025-09-25 22:07:42 -0300

    List.

---
+++ 11_ColecaoLista/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_ColecaoLista
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> nomes = new List<string>();
+
+           
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.Write($"Digite o {i}º nome: ");
+                string nome = Console.ReadLine();
+                nomes.Add(nome); 
+            }
+
+            Console.WriteLine("\n--- Nomes digitados ---");
+            foreach (string nome in nomes)
+            {
+                Console.WriteLine(nome);
+            }
+        }
+    }
+}
