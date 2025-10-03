05639671b41415a82a97600a442102e7e97f8629
Thiago Freitas <thifreitas26@gmail.com>
2025-10-02 22:20:57 -0300

    Validação de CPF

---
+++ _18_ValidaçãoCPF/Cpf.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _18_ValidaçãoCPF
+{
+    internal class CPF : IDocumento
+    {
+        private string numero; 
+
+        public CPF(string numero)
+        {
+            
+            this.numero = Regex.Replace(numero, "[^0-9]", "");
+        }
+
+     
+        public string ObterNumero()
+        {
+            return numero;
+        }
+
+        public bool Validar()
+        {
+            if (numero.Length != 11) return false;
+
+            if (new string(numero[0], 11) == numero) return false;
+
+            return numero[9] == CalculaDV(numero, 9, 10) &&
+                   numero[10] == CalculaDV(numero, 10, 11);
+        }
+
+        private char CalculaDV(string cpf, int qtdeNumeros, int peso)
+        {
+            int soma = 0;
+            for (int i = 0; i < qtdeNumeros; i++)
+                soma += (cpf[i] - '0') * (peso - i);
+
+            int resto = soma % 11;
+            int digito = (resto >= 2) ? 11 - resto : 0;
+
+            return (char)(digito + '0');
+        }
+    }
+}
+
+
---
+++ _18_ValidaçãoCPF/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+using _18_ValidaçãoCPF;
+
+
+namespace _08_ValidaCPF
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+
+            Console.Write("Digite seu CPF: ");
+            string cpfDigitado = Console.ReadLine();
+
+            IDocumento documento = new CPF(cpfDigitado);
+
+            if (documento.Validar())
+                Console.WriteLine("CPF VÁLIDO: " + documento.ObterNumero());
+            else
+                Console.WriteLine("CPF INVÁLIDO!");
+
+            Console.ReadKey();
+        }
+
+
+
+
+
+
+    }
+
+}
+    
+
