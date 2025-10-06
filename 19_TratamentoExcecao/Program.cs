c4dfa7858650c1c9710620e359f86940ca59ab5d
Thiago Freitas <thifreitas26@gmail.com>
2025-10-06 20:40:43 -0300

    Tratamento Exceções

---
+++ 19_TratamentoExcecao/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_TratamentoExcecao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            while (true)
+            {
+                Console.Write("Digite um número inteiro: ");
+                try
+                {
+                    numero = int.Parse(Console.ReadLine());
+                    break;
+                       
+                }
+                catch(FormatException ex)
+                {
+                    Console.WriteLine("Número inválido, informe apenas valores inteiros!\nPressione qualquer tecla para continuar");
+                    Console.ReadKey();  
+                }
+                finally
+                {
+                   Console.Clear(); 
+                }
+            }
+            if(numero % 2 == 0)
+                Console.WriteLine("O número é par");
+
+            Console.WriteLine("O número é ímpar");
+        }
+        
+    }
+}
